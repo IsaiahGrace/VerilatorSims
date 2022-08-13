0b92c0c1b6ea1f9248aa820afe1e72779be02ddd
Isaiah Grace <isaiah@graces.com>
2022-08-13 19:46:05 -0400

    Added gtest support, now we're cooking!

--- counter/main.cpp
+++ counter/main.cpp
@@ -1,24 +1,43 @@
 #include <iostream>
 
 #include "verilated.h"
+#include "gtest/gtest.h"
 
 #include "Testbench.hpp"
 
 #include "Vcounter.h"
 
-int main(int argc, char** argv) {
-	Verilated::commandArgs(argc, argv);
-
-    Testbench<Vcounter> tb;
-
-    tb.reset();
-
-    for (int i = 0; i < 300; i++) {
-    	tb.tick();
-	    std::cout << "Tick Count: [" << tb.tickCount << "] counter: [" << (int) tb.dev->count << "]" << std::endl;
+class CounterTestFixture: public ::testing::Test {
+ protected:
+    Testbench<Vcounter>* tb;
+    void SetUp() override {
+         tb = new Testbench<Vcounter>;
     }
 
-    tb.reset();
+    void TearDown() override {
+        delete tb;
+        tb = nullptr;
+    }
+};
 
-    return 0;
+TEST_F(CounterTestFixture, reset) {
+    tb->reset();
+    EXPECT_EQ(tb->dev->count, 0);
+}
+
+TEST_F(CounterTestFixture, counterOverFlow) {
+    tb->reset();
+
+    int ticks = 300;
+    for (int i = 0; i < ticks; i++) {
+        tb->tick();
+    }
+
+    EXPECT_EQ(tb->dev->count, ticks % (1 << 8));
+}
+
+int main(int argc, char** argv) {
+    Verilated::commandArgs(argc, argv);
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
